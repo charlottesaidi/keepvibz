46b7e264528c63bbd36b7552fdc7a944fc473cff
charlottesaidi <charlotte.saidi@outlook.fr>
2021-06-02 19:12:12 +0200

    install portal bootstrap

---
+++ assets/admin.js
@@ -0,0 +1,19 @@
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * We recommend including the built version of this JavaScript file
+ * (and its CSS file) in your base layout (base.html.twig).
+ */
+
+// any CSS you import will output into a single css file (app.css in this case)
+import './admin/css/portal.css';
+import './admin/plugins/fontawesome/js/all.min.js';
+import './admin/plugins/popper.min.js'
+import './admin/plugins/bootstrap/js/bootstrap.min.js'  
+import './admin/plugins/chart.js/chart.min.js'
+import './admin/js/index-charts.js' 
+import './admin/js/app.js'
+
+
+// start the Stimulus application
+import './bootstrap';

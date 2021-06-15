eff5db4beb32e5227f93d9704e67e4b102c9aee2
zelastwo <zelastwo@hotmail.fr>
2021-06-15 10:02:13 +0200

    Pagination instrus/lecteur audio

--- migrations/Version20210614083747.php
+++ migrations/Version20210614083747.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210614074153 extends AbstractMigration
+final class Version20210614083747 extends AbstractMigration
 {
     public function getDescription(): string
     {
